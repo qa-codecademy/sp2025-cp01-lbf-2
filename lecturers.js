aa34ebcd0aec71b5e65de785783b0e16e4aeb745
Alekssss7 <lawleyy7@gmail.com>
2025-06-12 15:57:08 +0200

    Delete lecturers.js

--- lecturers.js
+++
@@ -1,6 +0,0 @@
-  const toggle = document.getElementById("nav-toggle");
-  const navLinks = document.getElementById("nav-links");
-  
-  toggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
