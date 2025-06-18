3cf835f35d969dc93eda0c56812610e38538210e
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-06-18 23:46:14 +0200

    Homepage
    Added new sections to the homepage

--- home-page/homepage.js
+++ home-page/homepage.js
@@ -27,3 +27,19 @@ window.addEventListener("click", (e) => {
     document.body.style.overflow = "";
   }
 });
+
+// ScrollToTopBtn
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  };
+
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
