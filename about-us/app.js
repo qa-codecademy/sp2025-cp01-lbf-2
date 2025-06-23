d184991782340bea2eedf4f4a92aa7d55558ceec
David.P <davidpejoski15@gmail.com>
2025-06-23 23:00:07 +0200

    Footer Overhaul

--- about-us/app.js
+++ about-us/app.js
@@ -4,3 +4,41 @@
   toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
+
+   // ===== Contact modal =====
+const contactBtn = document.getElementById("openContactModal");
+const contactModal = document.getElementById("contactModal");
+const closeBtn = document.getElementById("closeContactModal");
+
+contactBtn.addEventListener("click", () => {
+  contactModal.style.display = "block";
+  document.body.style.overflow = "hidden";
+});
+
+closeBtn.addEventListener("click", () => {
+  contactModal.style.display = "none";
+  document.body.style.overflow = "";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === contactModal) {
+    contactModal.style.display = "none";
+    document.body.style.overflow = "";
+  }
+});
+
+// ScrollToTopBtn
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  };
+
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
