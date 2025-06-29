3f69dce8bfb928cc48fce88fad206d3a8ff417cf
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-06-29 09:24:30 +0200

    Contact options and new section

--- contact/app.js
+++ contact/app.js
@@ -1,22 +1,10 @@
+    // ===== Navbar toggle =====
   const toggle = document.getElementById("nav-toggle");
   const navLinks = document.getElementById("nav-links");
   
   toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
-
-///////////////SUBMIT BUTTON MESSAGE/////////////////
-// const form = document.getElementById('contactForm');
-// const tnx = document.getElementById('tnxMsg');
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//     form.style.display = 'none';
-//     tnx.style.display = 'block';
-//     setTimeout(() => {
-//       tnx.classList.add('show');
-//     }, 10);
-// });
-//////////////////////////////////////////////////////
 
    // ===== Contact modal =====
 const contactBtn = document.getElementById("openContactModal");
